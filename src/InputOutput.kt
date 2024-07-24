6fb221e3bbe1af6c6e6ac3e096363f620d35d96c
brucebett <bett6448@gmail.com>
2024-07-24 11:08:05 +0300

    weednesday

--- src/InputOutput.kt
+++ src/InputOutput.kt
@@ -1,3 +1,7 @@
+import java.time.LocalDate
+import java.util.*
+
+
 fun main(args: Array<String>) {
     println("please type your name here: ")
     var enterName = readLine()
@@ -16,11 +20,49 @@ fun main(args: Array<String>) {
 
 //    ask the user for two then do the summation and display it out
 
-    println("Enter First number:")
-    var num1 = readLine()
-    println("Enter Second number:")
-    var num2 = readLine()
-    var summation = num1 + num2
+    var read = Scanner(System.`in`)
+    println("Please enter first number:")
+    val firstNumber = read.nextFloat()
+    println("Please enter second number:")
+    val SecondNumber = read.nextFloat()
+    val sumAll = firstNumber + SecondNumber
+    println("The total is $sumAll")
 
-    println("Total = $summation")
+//    ask the user to key in the year of birth the performs arithematic
+//    to display there curent page
+
+
+
+
+            // Create a Scanner object for user input
+            val scanner = Scanner(System.`in`)
+
+            // Ask the user to enter their year of birth
+            print("Enter your year of birth: ")
+            val yearOfBirth = scanner.nextInt()
+
+            // Get the current date
+            val currentDate = LocalDate.now()
+
+            // Calculate the age
+            val currentYear = currentDate.year
+            val age = currentYear - yearOfBirth
+
+            // Display the current age
+            println("Your current age is: $age")
+
+            // Close the scanner
+            scanner.close()
+
+
+
+
+
+
+
+
+
+
+
+
 }
